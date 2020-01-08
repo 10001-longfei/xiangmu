ea4d3b564aab3b92d0b057fbc3acfe1ab7f1cfaf
sunny <13429422359@163.com>
2020-01-08 19:12:58 +0800

    update yangxia and Mrlong

--- hello.java
+++ hello.java
@@ -1,8 +1,15 @@
-//这是世界最伟大的发明
+﻿//这是世界最伟大的发明
 hello class  
 {
 	public static void main(String[] args) 
 	{   //打印出世界最美好的代码
 		System.out.println("你好 杨夏!");
 	}
+
+    public String test1（）{
+	   //打印出世界最美好的代码
+		System.out.println("你好 Mr隆!");
+	}
+
+
 }
